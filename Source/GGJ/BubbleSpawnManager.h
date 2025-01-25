d679b20aa0a0ed95ccde5353596982ab74b7892f
TDS-Poseidon <ts@poseidon.no>
2025-01-25 01:56:52 +0100

    Started to create bubble actor and manager.

---
+++ Source/GGJ/BubbleSpawnManager.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bubble.h"
+#include "Engine/TargetPoint.h"
+#include "GameFramework/Actor.h"
+#include "BubbleSpawnManager.generated.h"
+
+UCLASS()
+class GGJ_API ABubbleSpawnManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABubbleSpawnManager();
+
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category=BubbleManager)
+	TArray<TObjectPtr<ABubble>> Bubbles;
+UPROPERTY(EditAnywhere,BlueprintReadWrite,Category=BubbleManager)
+	AActor* SpawnStartLocation;
+	
+UPROPERTY(EditAnywhere,BlueprintReadWrite,Category=BubbleManager)
+	FVector SpawnPlayerBound;
+	
+UPROPERTY(EditAnywhere,BlueprintReadWrite,Category=BubbleManager)
+UStaticMeshComponent* LevelFloor;
+	
+UPROPERTY(EditAnywhere,BlueprintReadWrite,Category=BubbleManager)
+	int32 MaxBubblesAlive = 500;
+
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
