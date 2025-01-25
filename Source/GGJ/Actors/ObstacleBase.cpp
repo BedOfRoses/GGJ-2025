99b2e5177a40026e0535dafb1747d0e4572debfb
haldorj <b2z@live.no>
2025-01-25 05:44:55 +0100

    Obstacle base

---
+++ Source/GGJ/Actors/ObstacleBase.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ObstacleBase.h"
+
+#include "Components/SphereComponent.h"
+
+
+AObstacleBase::AObstacleBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	CollisionVolume = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionVolume"));
+	CollisionVolume->SetSphereRadius(100.0f);
+}
+
+void AObstacleBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AObstacleBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
